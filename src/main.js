b34e876b654450ebd323e5a524719f515dc104e8
volochayev <stingray8888831@gmail.com>
2025-06-11 19:31:08 +0300

    make some

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import axios from 'axios';
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+//знаходжу необхідне
+
 const form = document.querySelector(".form");
 const gallery = document.querySelector(".gallery");
 
+// додаю слухача подій на форму
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    
+})
